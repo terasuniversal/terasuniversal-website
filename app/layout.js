e62f126b4304cf5a0380ba2842058c6737632fb9
terasuniversal <admin@terasuniversal.com.my>
2026-07-17 11:52:57 +0800

    Self-host Montserrat and Poppins via next/font to remove render-blocking Google Fonts request

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import "./globals.css";
 import Analytics from "../components/Analytics";
+import { Montserrat, Poppins } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["600", "700", "800"], variable: "--font-montserrat", display: "swap" });
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-poppins", display: "swap" });
 
 export const viewport = {
   width: "device-width",
@@ -63,12 +67,9 @@ const websiteSchema = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700;800&family=Poppins:wght@400;500;600;700&display=swap" />
-        <link rel="prefetch" href="/about" />
+    <html lang="en" className={montserrat.variable + " " + poppins.variable}>
+        <head>
+  <link rel="prefetch" href="/about" />
         <link rel="prefetch" href="/services" />
         <link rel="prefetch" href="/training" />
         <link rel="prefetch" href="/contact" />
